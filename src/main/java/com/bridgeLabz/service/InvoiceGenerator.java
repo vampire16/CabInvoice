9836e170d2165ca4afe30b552d3aad0fe3a5fe76
vampire16 <hakevaibhav16@gmail.com>
2020-04-07 18:44:34 +0530

    ADDED : New method to calculate fare

--- src/main/java/com/bridgeLabz/service/InvoiceGenerator.java
+++ src/main/java/com/bridgeLabz/service/InvoiceGenerator.java
@@ -1,6 +1,13 @@
 package com.bridgeLabz.service;
 
 public class InvoiceGenerator {
+    private static final double COST_PER_KILOMETER = 10;
+    private static final int COST_PER_TIME = 1;
+
+    public double calculateFare(double distance, int time) {
+        return distance * COST_PER_KILOMETER + time * COST_PER_TIME;
+    }
+
     public static void main(String[] args){
         System.out.println("Welcome");
     }
