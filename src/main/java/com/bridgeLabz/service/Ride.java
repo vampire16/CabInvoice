f9e9850d15a74acd9da641975c094dca526635c8
vampire16 <hakevaibhav16@gmail.com>
2020-04-07 23:48:48 +0530

    ADDED : Class for rides

--- src/main/java/com/bridgeLabz/service/Ride.java
+++ src/main/java/com/bridgeLabz/service/Ride.java
@@ -1,4 +1,11 @@
 package com.bridgeLabz.service;
 
 public class Ride {
+    public final double distance;
+    public final int time;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
 }
