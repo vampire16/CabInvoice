9f6d940fdc5b264f144a782e9b7c2c8d496b1dea
vampire16 <hakevaibhav16@gmail.com>
2020-04-07 23:50:57 +0530

    ADDED : New test method to check multiple rides

--- src/test/java/InvoiceTestCases.java
+++ src/test/java/InvoiceTestCases.java
@@ -1,4 +1,5 @@
 import com.bridgeLabz.service.InvoiceGenerator;
+import com.bridgeLabz.service.Ride;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -20,5 +21,15 @@ public class InvoiceTestCases {
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance,time);
         Assert.assertEquals(5,fare,0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = { new Ride(2.0,5),
+                         new Ride(0.1,1)
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, fare, 0);
     }
 }
