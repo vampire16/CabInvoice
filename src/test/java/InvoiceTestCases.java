049fb311ddc6088ebd1db96dcafbcd4c399010bc
vampire16 <hakevaibhav16@gmail.com>
2020-04-07 23:26:03 +0530

    ADDED : New test method to check minimum fare

--- src/test/java/InvoiceTestCases.java
+++ src/test/java/InvoiceTestCases.java
@@ -12,4 +12,13 @@ public class InvoiceTestCases {
         double fare = invoiceGenerator.calculateFare(distance,time);
         Assert.assertEquals(25,fare,0);
     }
+
+    @Test
+    public void givenDistanceOrTime_ShouldReturnMinFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(5,fare,0);
+    }
 }
