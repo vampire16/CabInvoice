8e7195e47d3494a96751b53c770d838ef9502951
vampire16 <hakevaibhav16@gmail.com>
2020-04-07 23:25:19 +0530

    REFACTORED : If condition for minimum fare in calculateFare method

--- src/main/java/com/bridgeLabz/service/InvoiceGenerator.java
+++ src/main/java/com/bridgeLabz/service/InvoiceGenerator.java
@@ -3,9 +3,13 @@ package com.bridgeLabz.service;
 public class InvoiceGenerator {
     private static final double COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1;
+    private static final double MIN_FARE = 5;
 
     public double calculateFare(double distance, int time) {
-        return distance * COST_PER_KILOMETER + time * COST_PER_TIME;
+        double fare = distance * COST_PER_KILOMETER + time * COST_PER_TIME;
+        if(fare < MIN_FARE)
+            return MIN_FARE;
+        return fare;
     }
 
     public static void main(String[] args){
