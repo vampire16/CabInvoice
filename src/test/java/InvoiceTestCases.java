2b740fb053580aa6c3d0c8646a72d7ed9a0b71e3
vampire16 <hakevaibhav16@gmail.com>
2020-04-07 18:45:46 +0530

    ADDED : New test method to check total fare

--- src/test/java/InvoiceTestCases.java
+++ src/test/java/InvoiceTestCases.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import com.bridgeLabz.service.InvoiceGenerator;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class InvoiceTestCases {
+
+    @Test
+    public void givenDistanceAndTime__ThenReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(25,fare,0);
+    }
 }
