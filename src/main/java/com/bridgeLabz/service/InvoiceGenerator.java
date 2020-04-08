eb727ba09f8a524071878bf0cea4b610c1224da2
vampire16 <hakevaibhav16@gmail.com>
2020-04-08 23:06:52 +0530

    MODIFIED : Replace if statement with ternary operator

--- src/main/java/com/bridgeLabz/service/InvoiceGenerator.java
+++ src/main/java/com/bridgeLabz/service/InvoiceGenerator.java
@@ -7,9 +7,7 @@ public class InvoiceGenerator {
 
     public double calculateFare(double distance, int time) {
         double fare = distance * COST_PER_KILOMETER + time * COST_PER_TIME;
-        if(fare < MIN_FARE)
-            return MIN_FARE;
-        return fare;
+        return fare < MIN_FARE ? MIN_FARE : fare;
     }
 
     public static void main(String[] args){
