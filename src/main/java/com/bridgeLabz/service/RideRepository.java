85cc2b9b54274bab2e71751a0afc4f75ee0b399c
vampire16 <hakevaibhav16@gmail.com>
2020-04-09 09:52:34 +0530

    ADDED : New class for user ride

--- src/main/java/com/bridgeLabz/service/RideRepository.java
+++ src/main/java/com/bridgeLabz/service/RideRepository.java
@@ -1,4 +1,22 @@
 package com.bridgeLabz.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class RideRepository {
+    Map<String, ArrayList<Ride>> userRide;
+
+    public RideRepository() {
+        this.userRide = new HashMap<>();
+    }
+
+    public void userRides(String userId, Ride[] rides) {
+        this.userRide.put(userId, new ArrayList<>(Arrays.asList(rides)));
+    }
+
+    public Ride[] getRides(String userId) {
+        return this.userRide.get(userId).toArray(new Ride[0]);
+    }
 }
