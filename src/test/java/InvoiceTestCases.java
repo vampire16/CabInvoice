2e3ae167fb1e24f79ca6c40fc898513de79d6c8e
vampire16 <hakevaibhav16@gmail.com>
2020-04-09 09:53:35 +0530

    ADDED : New test method to check userId and rides

--- src/test/java/InvoiceTestCases.java
+++ src/test/java/InvoiceTestCases.java
@@ -11,24 +11,36 @@ public class InvoiceTestCases {
     public void givenDistanceAndTime__ThenReturnTotalFare() {
         double distance = 2.0;
         int time = 5;
-        double fare = invoiceGenerator.calculateFare(distance,time);
-        Assert.assertEquals(25,fare,0);
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0);
     }
 
     @Test
     public void givenDistanceOrTime_ShouldReturnMinFare() {
         double distance = 0.1;
         int time = 1;
-        double fare = invoiceGenerator.calculateFare(distance,time);
-        Assert.assertEquals(5,fare,0);
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0);
     }
 
     @Test
     public void givenMultipleRides_ShouldReturnSummary() {
-        Ride[] rides = { new Ride(2.0,5),
-                new Ride(0.1,1)
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
         };
         InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+        Assert.assertEquals(expectedSummary, summary);
+    }
+
+    @Test
+    public void givenUserIdAndRides_ShouldReturnSummary() {
+        String userId = "a@b.com";
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        invoiceGenerator.userRides(userId, rides);
+        InvoiceSummary summary = invoiceGenerator.getInvoiceSummary(userId);
         InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
         Assert.assertEquals(expectedSummary, summary);
     }
