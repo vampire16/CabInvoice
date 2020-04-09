710c3f494b64c6040acfe10444332e6872400666
vampire16 <hakevaibhav16@gmail.com>
2020-04-09 08:38:53 +0530

    REFACTORED : Changed return statement of calculateFare

--- src/main/java/com/bridgeLabz/service/InvoiceGenerator.java
+++ src/main/java/com/bridgeLabz/service/InvoiceGenerator.java
@@ -14,11 +14,11 @@ public class InvoiceGenerator {
         System.out.println("Welcome");
     }
 
-    public double calculateFare(Ride[] rides) {
+    public InvoiceSummary calculateFare(Ride[] rides) {
         double fare = 0;
         for(Ride ride:rides){
             fare += this.calculateFare(ride.distance, ride.time);
         }
-        return fare;
+        return new InvoiceSummary(rides.length, fare);
     }
 }
