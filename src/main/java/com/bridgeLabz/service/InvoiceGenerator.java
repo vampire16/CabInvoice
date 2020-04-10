76e1a41bec575dde597467ed0d240b905a3dbb3a
vampire16 <hakevaibhav16@gmail.com>
2020-04-10 08:32:48 +0530

    REFACTORED : If statement added for NORMAL_RIDE and PREMIUM_RIDE

--- src/main/java/com/bridgeLabz/service/InvoiceGenerator.java
+++ src/main/java/com/bridgeLabz/service/InvoiceGenerator.java
@@ -4,16 +4,30 @@ public class InvoiceGenerator {
     private static final double COST_PER_KILOMETER = 10;
     private static final int COST_PER_TIME = 1;
     private static final double MIN_FARE = 5;
+    private static final double PREMIUM_COST_PER_KILOMETER = 15;
+    private static final int PREMIUM_COST_PER_TIME = 2;
+    private static final double PREMIUM_MIN_FARE = 20;
+    private final TypesOfCab typesOfCab;
 
     RideRepository rideRepository = new RideRepository();
+
+    public InvoiceGenerator(TypesOfCab typesOfCab) {
+        this.typesOfCab = typesOfCab;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome");
     }
 
     public double calculateFare(double distance, int time) {
-        double fare = distance * COST_PER_KILOMETER + time * COST_PER_TIME;
-        return fare < MIN_FARE ? MIN_FARE : fare;
+        if(this.typesOfCab.equals(TypesOfCab.NORMAL_RIDE)) {
+            double fare = distance * COST_PER_KILOMETER + time * COST_PER_TIME;
+            return fare < MIN_FARE ? MIN_FARE : fare;
+        }else if(this.typesOfCab.equals(TypesOfCab.PREMIUM_RIDE)){
+            double fare = distance * PREMIUM_COST_PER_KILOMETER + time * PREMIUM_COST_PER_TIME;
+            return fare < PREMIUM_MIN_FARE ? PREMIUM_MIN_FARE : fare;
+        }
+        return 0;
     }
 
     public InvoiceSummary calculateFare(Ride[] rides) {
